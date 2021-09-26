8b825b479433adff0e59a9d9ac82223db79a2441
James <jamesmca@live.com>
2021-09-26 10:51:55 -0700

    cdnjs autosize.js

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express')
 const app = express()
 //view engine is ejs
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     const code = `Welcome to HasteBin!
-    
+
 Use the commands in the top right corner
 to create a new file to share with others.`
+
+    //, lineNumbers: code.split('\n').length but instead
+    //did it in main file
     res.render('code-display', { code })
+})
+
+app.get('/new', (req, res) => {
+    res.render("new")
 })
 
 app.listen(3000)
