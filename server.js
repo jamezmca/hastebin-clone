2a7b7c90e3b8b08403732b62e497926f69fbc18f
James <jamesmca@live.com>
2021-09-27 08:23:53 -0700

    Technicallly finished but experiencing a bug that is preventing form data from being accessed

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express()
 //view engine is ejs
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const Document = require('./models/Document')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/hastebin', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
 
 app.get('/', (req, res) => {
     const code = `Welcome to HasteBin!
@@ -12,11 +21,31 @@ to create a new file to share with others.`
 
     //, lineNumbers: code.split('\n').length but instead
     //did it in main file
-    res.render('code-display', { code })
+    res.render('code-display', { code, language: "plaintext" })
 })
 
 app.get('/new', (req, res) => {
     res.render("new")
 })
 
-app.listen(3000)
+app.post('/save', async (res, req) => {
+    const value = req.body.value
+    try {
+        const document = await Document.create({ value })
+        res.redirect(`/${document.id}`)
+    } catch (err) {
+        res.render('new', { value })
+    }
+})
+
+app.get('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const document = await Document.findById(id)
+        res.render('code-display', { code: document.value })
+    } catch (err) {
+        res.redirect('/')
+    }
+})
+
+app.listen(3000, () => console.log('connected'))
